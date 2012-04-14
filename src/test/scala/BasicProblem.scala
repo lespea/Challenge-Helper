052e0b3efa2a6dba2c08128213847f85c2c5b305
Adam Lesperance <lespea@gmail.com>
2012-04-14 00:40:56 -0500

    Use the new helper modules

--- src/test/scala/BasicProblem.scala
+++ src/test/scala/BasicProblem.scala
@@ -2,24 +2,20 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.ShouldMatchers
 import testProblem._
 import akka.actor._
-import challenge.ProblemMaster
+import challenge._
 import challenge.problem._
 
 class BasicProblem extends WordSpec with ShouldMatchers {
-  def buildInfo(pInfo: List[Tuple3[Int, Int, Int]]) = {
-    val (_, problems, answers) = pInfo.foldLeft(
-      new Tuple3[Int, List[AddProblem], Map[Int, String]](0, Nil, Map())){
-        case ((num, pList, aMap), info) ⇒
-          (
-            num + 1,
-            AddProblem(num, info._1, info._2) :: pList,
-            aMap + (num -> info._3.toString))
+  def buildInfo(pInfo: List[Tuple3[Int, Int, Int]]) =
+    pInfo.foldLeft(
+      new (List[AddProblem], Map[Int, String])(Nil, Map())){
+        case ((pList, aMap), info) ⇒
+          val problem = AddProblem(info._1, info._2)
+          (problem :: pList,
+            aMap + (problem.num -> info._3.toString))
       }
-    (problems, answers)
-  }
 
-  val system = ActorSystem("ChallengeSystemTest")
-  val master = system.actorOf(Props(ProblemMaster[AddProblem](5)), name = "problem_master_test")
+  val solver = new ProblemSolver[AddProblem](10, false)
 
   val okProblemInfo = List(
     (1, 1, 2),
@@ -28,14 +24,10 @@ class BasicProblem extends WordSpec with ShouldMatchers {
     (123, 456, 579),
     (5, 6, 11))
 
-  val badProblemInfo = List(
-    (1, 1, 0),
-    (2, 2, -3),
-    (1, 2, 43),
-    (123, 456, 9),
-    (5, 6, 734))
+  val badProblemInfo = okProblemInfo map { case (a, b, c) ⇒ (a, b, c + 1) }
 
   val (okProblems, okAnswers) = buildInfo(okProblemInfo)
+  NumGenerator.reset
   val (badProblems, badAnswers) = buildInfo(badProblemInfo)
 
   def okSolver = (sols: Iterable[SolvedProblem]) ⇒ sols foreach (sol ⇒
@@ -44,18 +36,30 @@ class BasicProblem extends WordSpec with ShouldMatchers {
   def badSolver = (sols: Iterable[SolvedProblem]) ⇒ sols foreach (sol ⇒
     Some(sol.answer) should not equal (badAnswers.get(sol.num)))
 
-  master ! ProblemInfo(okProblems, okSolver)
-  master ! ProblemInfo(badProblems, badSolver)
+  solver addInfo ProblemInfo(badProblems, badSolver)
 
   "A simple problem" should {
-    "solve correctly" in {
-      master ! ProcessProblem
+    "solve correctly" when {
+      "the answer is correct" in {
+        solver addInfo ProblemInfo(okProblems, okSolver)
+        solver solve
+      }
+
+      "the answer is incorrect" in {
+        Thread.sleep(100)
+        while (solver.isDone != true) Thread.sleep(100L)
+        solver addInfo ProblemInfo(okProblems, okSolver)
+        solver solve
+      }
     }
 
     "shutdown the actor system when it's finished" in {
-      Thread.sleep(1000)
-      master.isTerminated should be (true)
-      system.isTerminated should be (true)
+      Thread.sleep(500)
+      while (solver.isDone != true) Thread.sleep(100L)
+      solver.shutdown
+      Thread.sleep(100)
+      solver.master.isTerminated should be (true)
+      solver.system.isTerminated should be (true)
     }
   }
 }
--- src/test/scala/testProblem/addProblem.scala
+++ src/test/scala/testProblem/addProblem.scala
@@ -2,6 +2,6 @@ package testProblem
 
 import challenge.problem.Problem
 
-case class AddProblem(val num: Int, val add1: Int, val add2: Int) extends Problem {
+case class AddProblem(val add1: Int, val add2: Int) extends Problem {
   def solve: String = (add1 + add2).toString
 }
