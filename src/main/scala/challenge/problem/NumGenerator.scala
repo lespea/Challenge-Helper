5dfaef3b9171186b7ee423bfcc2948077f8d1d2b
Adam Lesperance <lespea@gmail.com>
2012-04-13 23:29:47 -0500

    Better abstraction

--- src/main/scala/challenge/problem/NumGenerator.scala
+++ src/main/scala/challenge/problem/NumGenerator.scala
@@ -4,10 +4,9 @@ import scala.concurrent.stm._
 
 object NumGenerator {
   val startNum = 0
+  def nextNum(num:Int) = num + 1
 
   final private[NumGenerator] val num = Ref(startNum)
-
-  def getNum = num.single getAndTransform { _ + 1 }
-  
+  final def getNum = num.single getAndTransform nextNum
   final def reset = num.single swap startNum
 }
