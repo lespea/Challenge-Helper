d7155c64e9dedc9a25efa9eb860e45a792ac1539
Adam Lesperance <lespea@gmail.com>
2012-04-14 00:40:17 -0500

    Helper function to test if the problems have been solved

--- src/main/scala/challenge/ProblemSolver.scala
+++ src/main/scala/challenge/ProblemSolver.scala
@@ -1,5 +1,9 @@
 package challenge
 import akka.actor._
+import akka.dispatch.Await
+import akka.pattern.ask
+import akka.util.Timeout
+import akka.util.duration._
 
 class ProblemSolver[A <: problem.Problem](workers: Int, autoDie: Boolean) {
   /**
@@ -13,4 +17,12 @@ class ProblemSolver[A <: problem.Problem](workers: Int, autoDie: Boolean) {
 
   def addInfo(info: problem.ProblemInfo[A]) = master ! info
   def solve = master ! problem.ProcessProblem
+
+  implicit val timeout = Timeout(5 seconds)
+  def isDone: Boolean = {
+    val future = master ? problem.DoneProcessing
+    Await.result(future, timeout.duration).asInstanceOf[Boolean]
+  }
+
+  def shutdown = system.shutdown
 }
