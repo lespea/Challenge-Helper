dadbfba48c1a9d98ac2221c16328dd8150c8f251
Adam Lesperance <lespea@gmail.com>
2012-04-13 21:20:40 -0500

    Ref should be a val

--- src/main/scala/challenge/problem/NumGenerator.scala
+++ src/main/scala/challenge/problem/NumGenerator.scala
@@ -5,7 +5,7 @@ import scala.concurrent.stm._
 object NumGenerator {
   val startNum = 0
 
-  var num = Ref(startNum)
+  val num = Ref(startNum)
 
   def getNum = num.single getAndTransform { _ + 1 }
   def reset = num.single swap startNum
